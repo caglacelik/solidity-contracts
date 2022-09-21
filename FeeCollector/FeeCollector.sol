fcad7d8ca0eb3ba7eb87432fbed38dda19c3fe0b
cagla <152120131104@ogrenci.ogu.edu.tr>
2022-09-21 23:51:21 +0300

    refactoring

--- FeeCollector/FeeCollector.sol
+++ FeeCollector/FeeCollector.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-    // Fee Collector is 
 contract FeeCollector { 
     
     address public owner; // owner of the contract
@@ -13,18 +12,19 @@ contract FeeCollector {
         owner = msg.sender; 
     }
     
-    // This function provides us adding the balance to the balance variable via coming from value property of the message object (ether)
+    // This function provides us adding the balance to the balance variable via coming from value property of the msg object
     receive() payable external {
         balance += msg.value; 
     }
 
+    // Modifier created for attaching the functions which checks the msg.sender is owner or not 
     modifier onlyOwner {
         require(msg.sender == owner, "Only owner can withdraw");
         _;
     }
     
-    // This function exists to allow us to send eth to the address which is one of the parameters of function => destAddr
-    // Decrease the balance of the contract => amount
+    // This function exists to allows us to send eth to the given address via parameter which is destAddr
+    // Decrease the balance of the contract via parameter which is amount
     function withdraw(uint amount, address payable destAddr) public onlyOwner {
         require(amount <= balance, "Insufficient funds");
         destAddr.transfer(amount);
