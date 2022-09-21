3291fffffe77b585b7cb0cd1e51222a87dc6a1fe
cagla <152120131104@ogrenci.ogu.edu.tr>
2022-09-21 17:03:23 +0300

    refactoring

---
+++ FeeCollector/FeeCollector.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+    // Fee Collector is 
+contract FeeCollector { 
+    
+    address public owner; // owner of the contract
+    uint256 public balance; // variable that we hold the balance of the contract
+    
+    // assigning msg.sender to the owner
+    constructor() {
+        owner = msg.sender; 
+    }
+    
+    // This function provides us adding the balance to the balance variable via coming from value property of the message object (ether)
+    receive() payable external {
+        balance += msg.value; 
+    }
+    
+    // This function exists to allow us to send eth to the address which is one of the parameters of function => destAddr
+    // Decrease the balance of the contract => amount
+    function withdraw(uint amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(amount <= balance, "Insufficient funds");
+        
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
