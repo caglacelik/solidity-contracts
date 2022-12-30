9ecf0eefd020bac8705ac12babf81aef5ff4a9c7
caglacelik <caglacelikkk@hotmail.com>
2022-12-30 14:45:51 +0000

    unnecessary comments deeleted

--- Counter/Counter.sol
+++ Counter/Counter.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 contract Counter {
 
-    // count variable
     uint private count;
 
     // This function is created to increment the count by 1
@@ -15,7 +14,6 @@ contract Counter {
 
     // This function is created to decrement the count by 1
     function dec() external returns(bool) {
-        // Count should be greater than zero to decrement its value
         require(count > 0, "Count is already at its min value: 0");
         count-=1;
         return true;
